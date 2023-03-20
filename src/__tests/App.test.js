ed986a39594aadb39aae4065018d2de1df361113
Carlos Marfil <carlos.marfil.r@gmail.com>
2023-03-20 18:48:59 +0000

    Testing App Routing, comments to share with Oliver

--- src/__tests/App.test.js
+++ src/__tests/App.test.js
@@ -1,8 +1,32 @@
-// import { render, screen } from '@testing-library/react';
-// import App from '../components/App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { createMemoryHistory } from "history";
+import React from "react";
+import { BrowserRouter, Router } from "react-router-dom";
+import "@testing-library/jest-dom";
+import { act } from "react-dom/test-utils";
+import App from "../components/App";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test("full app rendering/navigating", async () => {
+  const history = createMemoryHistory({ initialEntries: ["/"]["/lobby"] });
+  const setup = () => {
+    const utils = render(
+      <Router location={history.location} navigator={history}>
+        <App />
+      </Router>
+    );
+    const input = screen.getByLabelText("username-input");
+    return {
+      input,
+      ...utils,
+    };
+  };
+  const { input } = setup();
+
+  expect(screen.getByText(/Join the Conversation/i)).toBeInTheDocument(); // It is in Home
+
+  fireEvent.change(input, { target: { value: "Zufa" } }); // Adds input so it does not throw error
+  fireEvent.click(screen.getByText(/👍/i)); // Clicks button, it should navigate to "/lobby"
+
+  expect(screen.getByText(/Enter a room code/i).toBeInTheDocument); // It should be in Lobby
+});
